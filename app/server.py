7d270e27b906493eb0d90025d6e9ce41513592f1
new.warot <33780311+BlackCerulean@users.noreply.github.com>
2022-07-31 02:12:10 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,6 +7,7 @@ import os
 # from fastbook import *
 from fastai import *
 from fastai.imports import *
+from fastai.vision.image import *
 from fastai.vision.all import *
 from io import BytesIO
 from starlette.applications import Starlette
@@ -98,7 +99,7 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img = OpenImage(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
